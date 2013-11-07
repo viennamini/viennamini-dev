e6a6d16f63b390203c0233806ca1416839e5bb31
Josef Weinbub <weinbub@iue.tuwien.ac.at>
2013-11-07 19:57:31 +0100

    switched examples to external/ViennaDeviceCollection symbolic link

--- examples/trigate.cpp
+++ examples/trigate.cpp
@@ -28,58 +28,88 @@
 #include "viennamaterials/library.hpp"
 #include "viennamaterials/kernels/pugixml.hpp"
 
-const int left_contact    = 0;
-const int left_n          = 1;
-const int intrinsic       = 2;
-const int right_n         = 3;
-const int right_contact   = 4;
-
+const int source          = 0;
+const int channel         = 1;
+const int drain           = 2;
+const int oxide           = 3;
+const int gate_contact    = 4;
+const int body            = 5;
+const int body_contact    = 6;
+const int source_contact  = 7;
+const int drain_contact   = 8;
 
 /** @brief Structure the device by assigning 'roles', such as 'Oxide' to a segment.
     Also, assign a doping to the semiconductor regions */
 template<typename MeshT, typename SegmentationT, typename StorageT>
 void prepare(viennamini::device<MeshT, SegmentationT, StorageT>& device)
 {
-  // Segment 0:
-  device.assign_name          (left_contact, "left_contact");
-  device.assign_material      (left_contact, "Cu");
-  device.assign_contact       (left_contact);
+  // Source
+  device.assign_name          (source, "source");
+  device.assign_material      (source, "Si");
+  device.assign_semiconductor (source, 1e24, 1.E8);   // n+
 
-  // Segment 1:
-  device.assign_name          (left_n, "left_n");
-  device.assign_material      (left_n, "Si");
-  device.assign_semiconductor (left_n, 1.E24, 1.E8);
+  // Channel
+  device.assign_name          (channel, "channel");
+  device.assign_material      (channel, "Si");
+  device.assign_semiconductor (channel, 1.E12, 1.E20);  // p-substrate
 
-  // Segment 2:
-  device.assign_name          (intrinsic, "intrinsic");
-  device.assign_material      (intrinsic, "Si");
-  device.assign_semiconductor (intrinsic, 1.E21, 1.E11);
+  // Drain
+  device.assign_name          (drain, "drain");
+  device.assign_material      (drain, "Si");
+  device.assign_semiconductor (drain, 1.E24, 1.E8);     // n+
 
-  // Segment 3:
-  device.assign_name          (right_n, "right_n");
-  device.assign_material      (right_n, "Si");
-  device.assign_semiconductor (right_n, 1.E24, 1.E8);
+  // Oxide
+  device.assign_name          (oxide, "oxide");
+  device.assign_material      (oxide, "HfO2");
+  device.assign_oxide         (oxide);
 
-  // Segment 4:
-  device.assign_name          (right_contact, "right_contact");
-  device.assign_material      (right_contact, "Cu");
-  device.assign_contact       (right_contact);
+  // Gate Contact
+  device.assign_name          (gate_contact, "gate_contact");
+  device.assign_material      (gate_contact, "Cu");
+  device.assign_contact       (gate_contact);
+
+  // Body
+  device.assign_name          (body, "body");
+  device.assign_material      (body, "Si");
+  device.assign_semiconductor (body, 1.E12, 1.E20);  // p-substrate
+
+  // Body Contact
+  device.assign_name          (body_contact, "body_contact");
+  device.assign_material      (body_contact, "Cu");
+  device.assign_contact       (body_contact);
+
+  // Source Contact
+  device.assign_name          (source_contact, "source_contact");
+  device.assign_material      (source_contact, "Cu");
+  device.assign_contact       (source_contact);
+
+  // Drain Contact
+  device.assign_name          (drain_contact, "drain_contact");
+  device.assign_material      (drain_contact, "Cu");
+  device.assign_contact       (drain_contact);
+
 }
 
 /** @brief Assign actual values to the dirichlet contacts */
 void prepare_boundary_conditions(viennamini::config& config)
 {
-  // Segment 0:
-  config.assign_contact(left_contact, 0.0, 0.0);  // segment id, contact potential, workfunction
+  // Gate Contact
+  config.assign_contact(gate_contact, 0.4, 0.4);  // segment id, contact potential, workfunction
 
-  // Segment 4:
-  config.assign_contact(right_contact, 0.5, 0.0);
+  // Body Contact
+  config.assign_contact(body_contact, 0.0, 0.0);
+
+  // Source Contact
+  config.assign_contact(source_contact, 0.0, 0.0);
+
+  // Drain Contact
+  config.assign_contact(drain_contact, 0.4, 0.0);
 }
 
-int main()
+int main(int argc, char* argv[])
 {
   typedef double                                                       NumericType;
-  typedef viennagrid::mesh< viennagrid::config::triangular_2d >        MeshType;
+  typedef viennagrid::mesh< viennagrid::config::tetrahedral_3d >        MeshType;
   typedef viennagrid::result_of::segmentation<MeshType>::type          SegmentationType;
   typedef SegmentationType::segment_handle_type                        SegmentType;
   typedef viennadata::storage<>                                        StorageType;
@@ -94,7 +124,7 @@ int main()
   try
   {
     viennagrid::io::netgen_reader my_reader;
-    my_reader(mesh, segments,  "../examples/data/nin2d.mesh");
+    my_reader(mesh, segments, "../external/ViennaDeviceCollection/half-trigate3d/half-trigate3d.mesh");
   }
   catch (...)
   {
@@ -138,10 +168,10 @@ int main()
   config.temperature()                        = 300;
   config.damping()                            = 1.0;
   config.linear_breaktol()                    = 1.0E-13;
-  config.linear_iterations()                  = 700;
+  config.linear_iterations()                  = 1000;
   config.nonlinear_iterations()               = 100;
-  config.nonlinear_breaktol()                 = 1.0E-3;
-  config.initial_guess_smoothing_iterations() = 4;
+  config.nonlinear_breaktol()                 = 1.0E-2;
+  config.initial_guess_smoothing_iterations() = 5;
 
   //
   // Create a simulator object
@@ -155,11 +185,11 @@ int main()
   simulator();
 
   // Write results to vtk files
-  simulator.write_result("nin2d");
+  simulator.write_result("trigate");
 
 
   std::cout << "********************************************" << std::endl;
-  std::cout << "* NIN2D simulation finished successfully! *" << std::endl;
+  std::cout << "* TRIGATE simulation finished successfully! *" << std::endl;
   std::cout << "********************************************" << std::endl;
   return EXIT_SUCCESS;
 }
