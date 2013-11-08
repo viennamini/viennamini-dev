596568733c377438dd10b893c22e58b257315651
Josef Weinbub <weinbub@iue.tuwien.ac.at>
2013-11-08 14:44:05 +0100

    removed commented code in config.hpp

--- viennamini/config.hpp
+++ viennamini/config.hpp
@@ -42,34 +42,13 @@ struct config
   NumericType&  damping();
   IndexType&    initial_guess_smoothing_iterations();
 
-//  NumericType&  temperature()                         { return temperature_; }
-//  IndexType&    nonlinear_iterations()                { return nonlinear_iterations_; }
-//  NumericType&  nonlinear_breaktol()                  { return nonlinear_breaktol_; }
-//  IndexType&    linear_iterations()                   { return linear_iterations_; }
-//  NumericType&  linear_breaktol()                     { return linear_breaktol_; }
-//  NumericType&  damping()                             { return damping_; }
-//  IndexType&    initial_guess_smoothing_iterations()  { return initial_guess_smoothing_iterations_; }
-
   void assign_contact(std::size_t segment_index, NumericType value, NumericType workfunction);
-//  {
-//    segment_contact_values_       [segment_index] = value;
-//    segment_contact_workfunctions_[segment_index] = workfunction;
-//  }
 
   NumericType& contact_value(std::size_t segment_index);
-//  {
-//    return segment_contact_values_[segment_index];
-//  }
 
   NumericType& workfunction(std::size_t segment_index);
-//  {
-//      return segment_contact_workfunctions_[segment_index];
-//  }
 
   bool& drift_diffusion_state();
-//  {
-//    return model_drift_diffusion_state_;
-//  }
 
 private:
   IndexType         nonlinear_iterations_;
